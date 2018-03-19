9185de2313f0d2f35024434b4560d5b1673f6347
Josh Graber <joshgraber6@gmail.com>
2018-03-19 18:15:44 +0000

    Finished product no flashing

--- helloLCD.py/helloLCD.py
+++ helloLCD.py/helloLCD.py
@@ -2,12 +2,34 @@
 #
 
 import Adafruit_CharLCD as LCD
-
+import time
+import subprocess
 
 lcd = LCD.Adafruit_CharLCDPlate()
 
-displayText = "Hello World"
-
+Name = subprocess.check_output(['hostname']).strip()
+IPaddr = subprocess.check_output(['hostname', '-I'])
+displayText = "Hello World\n"
+displayText2 = IPaddr + Name
 lcd.clear()
 lcd.message(displayText)
+Refresh = True
+Wrefresh = True
 
+while (True):
+    if lcd.is_pressed(LCD.SELECT):
+        if Wrefresh:
+            lcd.set_backlight(1)
+            lcd.clear()
+            lcd.message(displayText)
+            Refresh = True
+            Wrefresh = False
+
+    else:
+        if Refresh:
+            lcd.set_backlight(1)
+            lcd.clear()
+            lcd.message(displayText2)
+            Refresh = False
+            Wrefresh = True
+    time.sleep(0.5)
